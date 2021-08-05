65835ed63bcbc53faf0a6270e41f8a982436c724
hirokiito0 <syu.itou.h@gmail.com>
2021-08-05 12:03:41 +0000

    ゲーム一覧画面、投稿画面作成

--- app/controllers/customer/games_controller.rb
+++ app/controllers/customer/games_controller.rb
@@ -1,10 +1,24 @@
 class Customer::GamesController < ApplicationController
   def index
+    @games = Game.all
   end
 
   def show
+    @game = Game.find_by(params[:id])
   end
 
   def new
+    @game = Game.new
   end
+  
+  def create
+    @game         = Game.new(games_params)
+    @game.user_id = current_user.id
+    @game.save
+    redirect_to customer_games_path
+  end
+  
+  def destroy
+  end
+  
 end
