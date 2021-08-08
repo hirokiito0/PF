fb2fb405fa2fc91c5de9b55ed0d353b126320a88
hirokiito0 <syu.itou.h@gmail.com>
2021-08-08 12:09:21 +0000

    会員詳細、編集機能追加

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,12 +1,33 @@
 class Customer::CustomersController < ApplicationController
+  before_action :ensure_correct_customer, only: [:update]
+  
   def show
     @customer = Customer.find(params[:id])
+    @games = @customer.games
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
   
   def updete
+    if @customer.update(customer_params)
+      redirect_to customers_path(@customer), notice: "You have updated user successfully."
+    else
+      render "show"
+    end
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_customer
+    @customer = Customer.find(params[:id])
+    unless @customer == current_customer
+      redirect_to customer_customers_path(current_customer)
+    end
   end
   
 end
